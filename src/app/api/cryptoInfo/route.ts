f73128fd6de52c8ec2a7992a891b781e90c12f6b
Ronald Schimpf <ronald.schimpf@uap.edu.ar>
2025-04-22 08:43:14 -0300

    solving deploy error

--- src/app/api/cryptoInfo/route.ts
+++ src/app/api/cryptoInfo/route.ts
@@ -1,4 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server';
+export interface CryptoDetailData {
+   id: string;
+   symbol: string;
+   name: string;
+   image: {
+      large: string;
+   };
+   market_data: {
+      current_price: {
+         usd: number;
+         ars: number;
+      };
+      price_change_percentage_24h: number;
+      price_change_percentage_7d: number;
+      price_change_percentage_30d: number;
+      market_cap: {
+         usd: number;
+      };
+      total_volume: {
+         usd: number;
+      };
+      circulating_supply: number;
+      total_supply: number;
+      max_supply: number;
+   };
+   description: {
+      en: string;
+   };
+   last_updated: string;
+}
 
 export async function GET(req: NextRequest) {
    const { searchParams } = new URL(req.url);
